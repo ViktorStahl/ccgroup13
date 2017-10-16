271995d7d735d15bf865b3216c5949f3b5120898
ViktorStahl <viktor.stahl.trash@gmail.com>
2017-10-16 19:40:20 +0200

    bug

--- benchres.m
+++ benchres.m
@@ -1,11 +1,11 @@
-Methods={'MC','MC-S','QMC-S','MLMC','MLMC-A',...    
-   'FFT','FGL','COS',...    
-   'FD','FD-NU','FD-AD',...    
-   'RBF','RBF-FD','RBF-PUM','RBF-LSML','RBF-AD','RBF-MLT'};
-problems={1,2,3,4}
-for i=1:numel(Methods) 
-	for j=numel(problems)
-		benchop(j,Methods{i})
-	end
-end
+%Methods={'MC','MC-S','QMC-S','MLMC','MLMC-A',...    
+%   'FFT','FGL','COS',...    
+%   'FD','FD-NU','FD-AD',...    
+%   'RBF','RBF-FD','RBF-PUM','RBF-LSML','RBF-AD','RBF-MLT'};
+%problems={1,2,3,4}
+%for i=1:numel(Methods) 
+%	for j=numel(problems)
+%		benchop(j,Methods{i})
+%	end
+%end
 res = benchop(4, 'FD');

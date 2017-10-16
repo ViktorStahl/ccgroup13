f85291816c082188aedfa0930447b0a4b0ff6b3e
ViktorStahl <viktor.stahl.trash@gmail.com>
2017-10-16 17:51:31 +0200

    testing

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def result_get():
 
 @app.route('/api/v1/result', methods=['POST'])
 def result_post():
-	app.logger.info(request.form)
+	app.logger.info(request.form['problem'])
 	result = swarmCaller.postResult(request.form)
 	return '0'
 

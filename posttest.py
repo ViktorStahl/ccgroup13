7d2dc028dac39830c2fb80890e800cf632e38cf6
ViktorStahl <viktor.stahl.trash@gmail.com>
2017-10-16 18:06:26 +0200

    Added testfile

---
+++ posttest.py
@@ -0,0 +1,14 @@
+def postResult(result):
+	with open('/home/ubuntu/result.txt', mode='a+') as file:
+		feeds={}
+		try:
+			feeds = json.load(file)
+		except:
+			pass
+		feeds.update(result)
+		json.dump(feeds, file)
+		file.close()
+		return '0'
+
+if __name__ == '__main__':
+	postResult({'1COS': {'problem': '1', 'time': '2.41', 'method': 'COS', 'result': '0.923'}})

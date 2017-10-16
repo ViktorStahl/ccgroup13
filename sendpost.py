159e8be626086bcf36e68e0682f7eb69f153204a
ViktorStahl <viktor.stahl.trash@gmail.com>
2017-10-16 10:21:42 +0200

    Added testing file

---
+++ sendpost.py
@@ -0,0 +1,6 @@
+import requests
+def post(api,problem, method, res, timeTaken):
+	r = requests.post("http://"+apiIP+":5000/api/v1/result", data={str(problem)+method:{'problem': problem, 'method': method, 'time': timeTaken, 'result': res})
+
+if __name__ == '__main__':
+	post('130.239.81.130', 1, 'COS', 0.923, 2.41)
